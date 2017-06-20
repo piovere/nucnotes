cfd6db9e419e70cf2ecdf6e112b56c3225fe29ed
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2017-06-20 16:00:50 -0400

    hw3 turned in

---
+++ ne697neuralnetworks/hw03/neuron_performance.m
@@ -0,0 +1,33 @@
+% Ask the user to input a data file of training data
+fn = input('Input the filename with training data: ', 's');
+% maybe verify to see if the file contained an "x" and a "y"
+% verify it contains exactly two matrices
+% verify that they have the same first dimension
+% verify their names are "x" and "y"
+% load the data
+load(fn)
+
+% MCUV scales the input data (call zscore1)
+[xs_mcuv, std_xs, mean_xs] = zscore1(x);
+[ys_mcuv, std_ys, mean_ys] = zscore1(y);
+
+n_array = [];
+p_array = [];
+
+for i = 1:99
+    net2 = feedforwardnet(i);
+    net2 = init(net2);
+    net2.trainParam.goal = 1e-10;
+    net2.performFcn = 'sse';
+    net2.divideFcn = 'dividetrain';
+    net2.layers{1}.transferFcn = 'logsig';
+    [net2, tr] = train(net2, xs_mcuv', ys_mcuv');
+    n_array = [n_array i];
+    p_array = [p_array tr.best_perf];
+    %clear net2;
+end
+
+loglog(n_array, p_array);
+title('Performance vs. Number of Neurons');
+xlabel('Neurons');
+ylabel('SSE');
--- ne697neuralnetworks/hw03/neuron_performance2.m
+++ ne697neuralnetworks/hw03/neuron_performance2.m
@@ -11,17 +11,23 @@ load(fn)
 [xs_mcuv, std_xs, mean_xs] = zscore1(x);
 [ys_mcuv, std_ys, mean_ys] = zscore1(y);
 
-% Trains the network using the trainlm method (this is done with the newff
-% and train functions)
-net = feedforwardnet(5);
-net = init(net);
-net.trainParam.goal = 1e-10;
-net.performFcn = 'sse';
-net.divideFcn = 'dividetrain';
-net.layers{1}.transferFcn = 'logsig';
-[net, tr] = train(net, xs_mcuv', ys_mcuv');
+%n_array = [];
+%p_array = [];
 
-% If training is successful (SSE < 1), saves the weight matrices in a file.
-if tr.best_tperf < 1
-    save('trainednetwork', 'net');
-end
+net2 = feedforwardnet(1);
+net2 = init(net2);
+net2.trainParam.goal = 1;
+net2.performFcn = 'sse';
+net2.divideFcn = 'dividetrain';
+net2.layers{1}.transferFcn = 'logsig';
+[net2, tr] = train(net2, xs_mcuv', ys_mcuv');
+%n_array = [n_array i];
+%p_array = [p_array tr.best_perf];
+clear net2;
+
+plt = semilogx(n_array, p_array);
+title('Performance vs. Number of Neurons');
+xlabel('Neurons');
+ylabel('SSE');
+
+saveas(plt, 'r
