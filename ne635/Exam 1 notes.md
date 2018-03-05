91e962d8065b5d608ac01f9c2e3d1bb5dc3df6fa
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2018-03-05 16:32:12 -0500

    started work on midterm too late!

---
+++ ne635/Exam 1 notes.md
@@ -0,0 +1,7 @@
+# Problem 1
+
+- [Pu dating with ratios](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4779457/)
+- [Counting rate with example](http://oregonstate.edu/instruct/ch374/ch418518/Chapter%203.pdf)
+- [Chemistry paper on Pu dating](https://pubs.acs.org/doi/pdf/10.1021/acs.analchem.7b05204)
+- [General solution to Bateman equations](http://metronu.ulb.ac.be/npauly/art_2013_2014/cetnar_2006.pdf)
+
---
+++ ne635/e1p1.py
@@ -0,0 +1,40 @@
+import math
+from random import seed, random
+
+class Nuclide(object):
+    def __init__(self, name, daughter, halflife, activity=None, amount=None):
+        self._halflife = halflife
+        self._daughter = daughter
+        self._name = name
+
+        if activity is None:
+            self._amount = amount
+            self._activity = amount * math.log(2) / self._halflife
+        elif amount is None:
+            self._activity = activity
+            self.amount = activity * self._halflife / math.log(2)
+
+    
+    def repr(self):
+        return self._name
+    
+
+    def set_activity(self, activity):
+        self.set_lambda()
+
+
+    def set_lambda(self):
+        self._lambda = math.log(2) / self._halflife
+
+
+    def decay(self, time):
+        self.set_lambda()
+        frac = math.exp(self._lambda) * time
+        daughter = (1 - frac) * self._amount
+        self._amount = frac * self._amount
+        return daughter
+
+
+sample = {
+    94241: Nuclide(94241, 95241, 4.509581e8, activity=3.714e12)
+}
