5e9b4322ec7852bc4924f563938045da9cab649c
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2018-10-07 21:31:46 -0500

    comparing PCR and PLS

--- ne579/Homework/hw05/hw05.py
+++ ne579/Homework/hw05/hw05.py
@@ -4,6 +4,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.pipeline import Pipeline
 from utilities import load_matlab_data, train_test_val_split, rmse
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
 
 
 # Set seed to match earlier homeworks
@@ -29,14 +31,50 @@ for i in range(1, x.shape[1]):
     e = rmse(yts, p.predict(xts))
     print(f"Error for {i} components: {e}")
 
-models = [PLSRegression(n_components=i).fit(xtr, ytr) for i in range(1, x.shape[1]+1)]
-# map(lambda m: m.fit(xtr, ytr), models)
+pls_models = [PLSRegression(n_components=i).fit(xtr, ytr) for i in range(1, x.shape[1]+1)]
+# map(lambda m: m.fit(xtr, ytr), pls_models)
 
-es = [rmse(yts, m.predict(xts)) for m in models]
+es = [rmse(yts, m.predict(xts)) for m in pls_models]
 
 f = plt.figure()
 plt.plot(range(1, x.shape[1]+1), es)
 plt.xlabel("Number of components")
 plt.ylabel("Test set RMSE")
 plt.savefig("images/rmse_vs_number_components.png", dpi=1000)
+plt.show()
+
+def PCR(n_features):
+    pcr = Pipeline([
+        ('scale', StandardScaler()),
+        ('pca', PCA(n_components=n_features)),
+        ('linear', LinearRegression())
+    ])
+    return pcr
+
+pcr_models = [PCR(i).fit(xtr, ytr) for i in range(1, x.shape[1]+1)]
+es = [rmse(yts, m.predict(xts)) for m in pcr_models]
+
+f = plt.figure()
+plt.plot(range(1, x.shape[1]+1), es)
+plt.xlabel("Number of PCA loadings")
+plt.ylabel("PCR RMSE")
+plt.savefig("images/rmse_vs_pca_loadings.png", dpi=1000)
+plt.show()
+
+f, axes = plt.subplots(nrows=5, ncols=2, sharey='row')
+
+tmp = pls_models[4]
+tmp2 = pcr_models[4]
+
+for i in range(5):
+    l = tmp.x_loadings_.T[i]
+    ax = axes[i, 0]
+    ax.bar(x=range(l.shape[0]), height=l)
+
+for i in range(5):
+    l = tmp2.named_steps.pca.components_[i]
+    ax = axes[i, 1]
+    ax.bar(x=range(len(l)), height=l)
+
+plt.savefig("images/pls_vs_pcr_loadings.png", dpi=1000)
 plt.show()
