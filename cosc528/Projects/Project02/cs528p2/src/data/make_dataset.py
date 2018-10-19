c4868d5bdb23ad06e0ba8202245a6b04ffc2090a
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2018-10-19 14:45:46 -0400

    data loading

--- cosc528/Projects/Project02/cs528p2/src/data/make_dataset.py
+++ cosc528/Projects/Project02/cs528p2/src/data/make_dataset.py
@@ -3,6 +3,8 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
 
 
 @click.command()
@@ -12,6 +14,45 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    df = pd.read_excel(input_filepath, na_values=["-"],
+                   nrows=57, true_values=["x"])
+    # Fill in missing values from research
+    # Add Clemson IPEDS number
+    ix = np.where(df["Name"] == "Clemson Univ.")[0][0]
+    df.at[ix, "IPEDS#"] = 217882
+
+    # Drop "HBC" since it has no variance
+    # Drop "Med School Res $" since it's missing for many schools
+    # Drop "AG Research ($000)" since it's missing for many schoools
+    # Drop "Wall St. Jourl Rank" since it's missing for many schoools
+    drop_columns = [
+        "HBC", 
+        "Med School Res $",
+        "AG Research ($000)",
+        "Wall St. Jourl Rank"
+    ]
+    df_clean = df.drop(drop_columns, 'columns')
+
+    # Some of these columns are categorical.
+    # Convert them to one-hot
+    categorical_columns = [
+        "Carm R1",
+        "2014 Med School",
+        "Vet School",
+    ]
+    df_clean = pd.get_dummies(df_clean, columns=categorical_columns)
+
+    # Clemson endowment in 2017 was $608,000,000
+    # Source: https://www.clemson.edu/giving/cufoundations/documents/allocations.pdf
+    ix = np.where(df["Name"] == "Clemson Univ.")[0][0]
+    df_clean.at[ix, "Endowment Figure"] = 608000000
+    df_clean.at[ix, "Endowment"] = 608000000
+    df_clean.at[ix, "Enowment / St. FTE"] = \
+        df_clean.at[ix, "Endowment"] / df_clean.at[ix, "ST. FTE"]
+
+    # Save to file
+    df_clean.to_csv(output_filepath)
+
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
@@ -27,4 +68,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    main("data/raw/UTK-peers.xls", "data/processed/UTK-peers_processed.csv")
